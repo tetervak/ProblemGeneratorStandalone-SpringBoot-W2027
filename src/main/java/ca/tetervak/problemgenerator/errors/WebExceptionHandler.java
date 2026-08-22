99b368f7fcce001a3a54ce6cce83a1670173ac1e
Alex Tetervak <tetervak@sheridancollege.ca>
2026-08-22 19:53:15 -0400

    added log to the exception handler

--- src/main/java/ca/tetervak/problemgenerator/errors/WebExceptionHandler.java
+++ src/main/java/ca/tetervak/problemgenerator/errors/WebExceptionHandler.java
@@ -1,5 +1,6 @@
 package ca.tetervak.problemgenerator.errors;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,12 +8,14 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
+@Slf4j
 @ControllerAdvice
 public class WebExceptionHandler {
 
     @ExceptionHandler(NoResourceFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND) // Ensures 404 statuses are set
     public Object handleNoResourceFoundException(NoResourceFoundException ex) {
+        log.error(ex.getMessage(), ex);
         String path = ex.getResourcePath();
         ModelAndView mav = new ModelAndView("error/error-404");
         mav.addObject("method", ex.getHttpMethod());
@@ -22,6 +25,7 @@ public class WebExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ModelAndView handleException(Exception ex) {
+        log.error(ex.getMessage(), ex);
         ModelAndView mav = new ModelAndView("error/general-error");
         mav.addObject("exception", ex.getClass().getSimpleName());
         mav.addObject("message", ex.getMessage());
